c954e385ea950cd91317f7d6656c9d29ac36cb96
UPMS <upms.ellora@gmail.com>
2020-08-16 15:53:55 +0530

    Final Commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,322 @@
+import 'package:flutter/material.dart';
+import 'package:music_app/models/playlist.dart';
+import 'package:music_app/models/song.dart';
+import 'package:music_app/constants.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: kWhiteColor,
+      appBar: AppBar(
+        title: Text(
+          'Discover',
+          style: Theme.of(context).textTheme.headline4.copyWith(
+                fontWeight: FontWeight.bold,
+                color: kPrimaryColor,
+              ),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.account_circle,
+              color: kPrimaryColor,
+              size: 30.0,
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 0.72 * size.height,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [_buildNavigationRail(), _buildPlaylistAndSongs(size)],
+            ),
+          ),
+          _buildCurrentPlayingSong(size),
+          _buildBottomBar(size)
+        ],
+      ),
+      bottomSheet: Container(
+        height: size.height * 0.4,
+        color: Colors.blueAccent,
+        child: Text("djghsdkhgsd"),
+      ),
+    );
+  }
+
+  GestureDetector _buildCurrentPlayingSong(Size size) {
+    print(80 / size.height);
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/song');
+      },
+      child: Container(
+        height: size.height * 0.103,
+        padding: EdgeInsets.symmetric(horizontal: 40.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(50),
+            topRight: Radius.circular(50),
+          ),
+          color: kSecondaryColor,
+        ),
+        child: Row(children: [
+          Hero(
+            tag: "SongIconImage",
+            child: CircleAvatar(
+              radius: 25,
+              backgroundImage: AssetImage('assets/images/colors.jpg'),
+            ),
+          ),
+          SizedBox(
+            width: 10.0,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Rewrite the stars',
+                style: TextStyle(
+                  color: kPrimaryColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+              Text(
+                'Zac Effron',
+                style: TextStyle(color: kWhiteColor, fontSize: 12.0),
+              ),
+            ],
+          ),
+          Expanded(child: Container()),
+          Icon(
+            Icons.favorite_border,
+            color: kPrimaryColor,
+          ),
+          SizedBox(width: 10.0),
+          Container(
+            height: 30,
+            width: 30,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0), color: Colors.white),
+            child: Icon(
+              Icons.pause,
+              color: kPrimaryColor,
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+
+  Column _buildPlaylistAndSongs(Size size) {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 0.35 * size.height,
+          width: size.width * 0.8,
+          // color: Colors.purple,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: playlists.length,
+            itemBuilder: (context, index) => _buildPlaylistItem(
+                image: playlists[index].image,
+                title: playlists[index].playlistName),
+          ),
+        ),
+        Container(
+          height: 0.35 * size.height,
+          width: size.width * 0.8,
+          child: ListView.builder(
+            itemCount: songs.length,
+            itemBuilder: (context, index) => _buildSonglistItem(
+              image: songs[index].image,
+              title: songs[index].songName,
+              subtitle: songs[index].artist,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  NavigationRail _buildNavigationRail() {
+    return NavigationRail(
+      minWidth: 56.0,
+      selectedIndex: _selectedIndex,
+      onDestinationSelected: (int index) {
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+      groupAlignment: -0.1,
+      labelType: NavigationRailLabelType.all,
+      leading: Column(
+        children: [
+          Icon(
+            Icons.playlist_play,
+            color: kPrimaryColor,
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          RotatedBox(
+            quarterTurns: -1,
+            child: Text(
+              "Your playlists",
+              style:
+                  TextStyle(color: kPrimaryColor, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+      selectedLabelTextStyle:
+          TextStyle(color: kPrimaryColor, fontWeight: FontWeight.bold),
+      unselectedLabelTextStyle:
+          TextStyle(color: kLightColor, fontWeight: FontWeight.bold),
+      destinations: [
+        NavigationRailDestination(
+          icon: SizedBox.shrink(),
+          label: RotatedBox(
+            quarterTurns: -1,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Recent"),
+            ),
+          ),
+        ),
+        NavigationRailDestination(
+          icon: SizedBox.shrink(),
+          label: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: RotatedBox(
+              quarterTurns: -1,
+              child: Text("Like"),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Container _buildBottomBar(Size size) {
+    print(50 / size.height);
+    return Container(
+      height: size.height * 0.065,
+      color: kSecondaryColor,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(50),
+              topRight: Radius.circular(50),
+            ),
+            color: kWhiteColor),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(
+              Icons.home,
+              color: kLightColor,
+            ),
+            Icon(
+              Icons.search,
+              color: kLightColor,
+            ),
+            Icon(
+              Icons.playlist_play,
+              color: kLightColor,
+            ),
+            Icon(
+              Icons.favorite_border,
+              color: kLightColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPlaylistItem({String title, String image}) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+      width: 220,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        image: DecorationImage(image: AssetImage(image), fit: BoxFit.fill),
+        // color: Colors.orange,
+      ),
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(12.0),
+              child: Text(
+                title,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.white),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                height: 0,
+              ),
+            ),
+            Container(
+              height: 30,
+              width: 30,
+              margin: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white),
+              child: Icon(
+                Icons.play_circle_outline,
+                color: kPrimaryColor,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSonglistItem({String image, String title, String subtitle}) {
+    return ListTile(
+      title: Text(
+        title,
+        style: TextStyle(color: kPrimaryColor),
+      ),
+      subtitle: Text(
+        subtitle,
+        style: TextStyle(color: kLightColor),
+      ),
+      leading: Container(
+        height: 50,
+        width: 50,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(image),
+              fit: BoxFit.fill,
+            ),
+            borderRadius: BorderRadius.circular(10.0)),
+      ),
+    );
+  }
+}
---
+++ lib/screens/song_screen.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:music_app/constants.dart';
+
+class SongScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: kSecondaryColor,
+      appBar: AppBar(
+        leading: GestureDetector(
+          onTap: () => Navigator.pop(context),
+          child: Icon(
+            Icons.arrow_back,
+            color: kPrimaryColor,
+          ),
+        ),
+        title: Text(
+          'Now playing',
+          style: TextStyle(
+              fontSize: 15.0,
+              color: kPrimaryColor,
+              fontWeight: FontWeight.w800),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        actions: [
+          Icon(
+            Icons.more_horiz,
+            color: kPrimaryColor,
+          ),
+          SizedBox(width: 5.0)
+        ],
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: kWhiteColor,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(35.0),
+            topRight: Radius.circular(35.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Hero(
+              tag: "SongIconImage",
+              child: Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: size.height * 0.4,
+                  child: Center(
+                    child: Container(
+                      margin: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      constraints: BoxConstraints(
+                          maxHeight: size.width * 0.45,
+                          maxWidth: size.width * 0.45),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/meditation.jpg'),
+                            fit: BoxFit.fill),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                children: [
+                  Text(
+                    'Rewrite the stars',
+                    style: TextStyle(
+                        color: kPrimaryColor,
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Expanded(
+                    child: Container(),
+                  ),
+                  Icon(
+                    Icons.favorite,
+                    color: favoriteColor,
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Text(
+                'Zac Effron',
+                style: TextStyle(
+                  color: kLightColor,
+                  fontSize: 15.0,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              width: double.infinity,
+              child: LinearProgressIndicator(
+                backgroundColor: kLightColor2,
+                value: 0.6,
+                valueColor: AlwaysStoppedAnimation(kPrimaryColor),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                children: [
+                  Text(
+                    '04:30',
+                    style: TextStyle(
+                        color: kLightColor,
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Expanded(
+                    child: Container(),
+                  ),
+                  Text(
+                    '06:30',
+                    style: TextStyle(
+                        color: kLightColor,
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10.0),
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.playlist_add,
+                    size: 0.09 * size.width,
+                    color: kLightColor,
+                  ),
+                  Icon(
+                    Icons.skip_previous,
+                    size: 0.12 * size.width,
+                    color: kPrimaryColor,
+                  ),
+                  Icon(
+                    Icons.play_circle_outline,
+                    color: kPrimaryColor,
+                    size: 0.18 * size.width,
+                  ),
+                  Icon(
+                    Icons.skip_next,
+                    size: 0.12 * size.width,
+                    color: kPrimaryColor,
+                  ),
+                  Icon(
+                    Icons.swap_horiz,
+                    size: 0.09 * size.width,
+                    color: kLightColor,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
